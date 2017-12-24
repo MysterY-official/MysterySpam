10bc62a77cac13b30675072f86d476bed26cb861
Leon <34793090+MysterY-official@users.noreply.github.com>
2017-12-24 16:00:00 +0100

    MysterySpam is now usable with 2 modes!

--- MysterySpam/GUI.Designer.cs
+++ MysterySpam/GUI.Designer.cs
@@ -32,10 +32,19 @@
             this.InfoButton = new MaterialSkin.Controls.MaterialRaisedButton();
             this.SpamText = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.StartStopLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.SpeedTrackBar = new System.Windows.Forms.TrackBar();
+            this.SpeedLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.ModeLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.CopyPasteModeButton = new MaterialSkin.Controls.MaterialRadioButton();
+            this.SimulateModeButton = new MaterialSkin.Controls.MaterialRadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // InfoButton
             // 
+            this.InfoButton.AutoSize = true;
+            this.InfoButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.InfoButton.Depth = 0;
             this.InfoButton.Icon = null;
             this.InfoButton.Location = new System.Drawing.Point(536, 352);
@@ -64,6 +73,7 @@
             this.SpamText.TabIndex = 1;
             this.SpamText.TabStop = false;
             this.SpamText.UseSystemPasswordChar = false;
+            this.SpamText.TextChanged += new System.EventHandler(this.SpamText_Change);
             // 
             // StartStopLabel
             // 
@@ -71,12 +81,98 @@
             this.StartStopLabel.Depth = 0;
             this.StartStopLabel.Font = new System.Drawing.Font("Roboto", 11F);
             this.StartStopLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.StartStopLabel.Location = new System.Drawing.Point(12, 360);
+            this.StartStopLabel.Location = new System.Drawing.Point(13, 369);
             this.StartStopLabel.MouseState = MaterialSkin.MouseState.HOVER;
             this.StartStopLabel.Name = "StartStopLabel";
             this.StartStopLabel.Size = new System.Drawing.Size(194, 19);
             this.StartStopLabel.TabIndex = 2;
             this.StartStopLabel.Text = "Press F8 to start/F9 to stop";
+            // 
+            // SpeedTrackBar
+            // 
+            this.SpeedTrackBar.LargeChange = 50;
+            this.SpeedTrackBar.Location = new System.Drawing.Point(12, 110);
+            this.SpeedTrackBar.Maximum = 1000;
+            this.SpeedTrackBar.Minimum = 100;
+            this.SpeedTrackBar.Name = "SpeedTrackBar";
+            this.SpeedTrackBar.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.SpeedTrackBar.Size = new System.Drawing.Size(576, 45);
+            this.SpeedTrackBar.SmallChange = 10;
+            this.SpeedTrackBar.TabIndex = 3;
+            this.SpeedTrackBar.TickFrequency = 2500;
+            this.SpeedTrackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.SpeedTrackBar.Value = 250;
+            this.SpeedTrackBar.Scroll += new System.EventHandler(this.SpeedTrackBar_Scroll);
+            // 
+            // SpeedLabel
+            // 
+            this.SpeedLabel.AutoSize = true;
+            this.SpeedLabel.Depth = 0;
+            this.SpeedLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.SpeedLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.SpeedLabel.Location = new System.Drawing.Point(249, 158);
+            this.SpeedLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.SpeedLabel.Name = "SpeedLabel";
+            this.SpeedLabel.Size = new System.Drawing.Size(103, 19);
+            this.SpeedLabel.TabIndex = 4;
+            this.SpeedLabel.Text = "Speed: 250ms";
+            // 
+            // ModeLabel
+            // 
+            this.ModeLabel.AutoSize = true;
+            this.ModeLabel.Depth = 0;
+            this.ModeLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.ModeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ModeLabel.Location = new System.Drawing.Point(8, 271);
+            this.ModeLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ModeLabel.Name = "ModeLabel";
+            this.ModeLabel.Size = new System.Drawing.Size(47, 19);
+            this.ModeLabel.TabIndex = 5;
+            this.ModeLabel.Text = "Mode";
+            // 
+            // CopyPasteModeButton
+            // 
+            this.CopyPasteModeButton.AutoSize = true;
+            this.CopyPasteModeButton.Checked = true;
+            this.CopyPasteModeButton.Depth = 0;
+            this.CopyPasteModeButton.Font = new System.Drawing.Font("Roboto", 10F);
+            this.CopyPasteModeButton.Location = new System.Drawing.Point(12, 290);
+            this.CopyPasteModeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.CopyPasteModeButton.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.CopyPasteModeButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.CopyPasteModeButton.Name = "CopyPasteModeButton";
+            this.CopyPasteModeButton.Ripple = true;
+            this.CopyPasteModeButton.Size = new System.Drawing.Size(195, 30);
+            this.CopyPasteModeButton.TabIndex = 6;
+            this.CopyPasteModeButton.TabStop = true;
+            this.CopyPasteModeButton.Text = "Copy/Paste Mode (fastest)";
+            this.CopyPasteModeButton.UseVisualStyleBackColor = true;
+            this.CopyPasteModeButton.CheckedChanged += new System.EventHandler(this.CopyPasteModeButton_CheckedChanged);
+            // 
+            // SimulateModeButton
+            // 
+            this.SimulateModeButton.AutoSize = true;
+            this.SimulateModeButton.Depth = 0;
+            this.SimulateModeButton.Font = new System.Drawing.Font("Roboto", 10F);
+            this.SimulateModeButton.Location = new System.Drawing.Point(12, 320);
+            this.SimulateModeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.SimulateModeButton.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.SimulateModeButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.SimulateModeButton.Name = "SimulateModeButton";
+            this.SimulateModeButton.Ripple = true;
+            this.SimulateModeButton.Size = new System.Drawing.Size(369, 30);
+            this.SimulateModeButton.TabIndex = 7;
+            this.SimulateModeButton.Text = "Simulate Mode (slower, but should work in every game)";
+            this.SimulateModeButton.UseVisualStyleBackColor = true;
+            this.SimulateModeButton.CheckedChanged += new System.EventHandler(this.SimulateModeButton_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(4, 263);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(385, 94);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
             // 
             // GUI
             // 
@@ -84,9 +180,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.SimulateModeButton);
+            this.Controls.Add(this.CopyPasteModeButton);
+            this.Controls.Add(this.ModeLabel);
+            this.Controls.Add(this.SpeedLabel);
+            this.Controls.Add(this.SpeedTrackBar);
             this.Controls.Add(this.StartStopLabel);
             this.Controls.Add(this.SpamText);
             this.Controls.Add(this.InfoButton);
+            this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "GUI";
@@ -95,7 +197,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MysterySpam - V";
             this.TopMost = true;
-            this.Load += new System.EventHandler(this.GUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedTrackBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +208,12 @@
         private MaterialSkin.Controls.MaterialRaisedButton InfoButton;
         private MaterialSkin.Controls.MaterialSingleLineTextField SpamText;
         private MaterialSkin.Controls.MaterialLabel StartStopLabel;
+        private System.Windows.Forms.TrackBar SpeedTrackBar;
+        private MaterialSkin.Controls.MaterialLabel SpeedLabel;
+        private MaterialSkin.Controls.MaterialLabel ModeLabel;
+        private MaterialSkin.Controls.MaterialRadioButton CopyPasteModeButton;
+        private MaterialSkin.Controls.MaterialRadioButton SimulateModeButton;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- MysterySpam/GUI.cs
+++ MysterySpam/GUI.cs
@@ -4,13 +4,20 @@ using System;
 using System.Windows.Forms;
 using System.Windows.Input;
 using GlobalHotKey;
+using System.Threading;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace MysterySpam
 {
     public partial class GUI : MaterialForm
     {
-        private bool Running = false;
-        String VersionString = "0.1";
+        String VersionString = "0.2";
+        bool Running = false;
+        static bool CopyPasteMode = true;
+        System.Threading.Timer Timer = new System.Threading.Timer(Timer_callback, null, Timeout.Infinite, Timeout.Infinite);
+        static InputSimulator Input = new InputSimulator();
+        static String SpamText2 = "";
 
         public GUI()
         {
@@ -39,23 +46,93 @@ namespace MysterySpam
             if (e.HotKey.Key == Key.F8)
             {
                 Running = true;
+                if (!SpamText.Text.Equals(""))
+                {
+                    Clipboard.SetText(SpamText.Text);
+                }
+                else
+                {
+                    Clipboard.Clear();
+                }
+                SpamText2 = SpamText.Text;
+                Timer.Change(0, SpeedTrackBar.Value);
             }
 
             if (e.HotKey.Key == Key.F9)
             {
                 Running = false;
+                Timer.Change(Timeout.Infinite, Timeout.Infinite);
             }
-        }
-
-        private void GUI_Load(object sender, EventArgs e)
-        {
-
         }
 
         private void InfoButton_Click(object sender, EventArgs e)
         {
             Form InfoGUI = new InfoGUI();
             InfoGUI.ShowDialog(this);
+        }
+
+        private void SpeedTrackBar_Scroll(object sender, EventArgs e)
+        {
+            if (Running)
+            {
+                Timer.Change(0, SpeedTrackBar.Value);
+            }
+            SpeedLabel.Text = "Speed: " + SpeedTrackBar.Value + "ms";
+        }
+
+        private static void Timer_callback(object state)
+        {
+            if (CopyPasteMode)
+            {
+                Input.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
+                Input.Keyboard.KeyDown(VirtualKeyCode.RETURN);
+            }
+            else
+            {
+                Input.Keyboard.TextEntry(SpamText2);
+                Input.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            }
+        }
+
+        private void CopyPasteModeButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CopyPasteModeButton.Checked)
+            {
+                CopyPasteMode = true;
+                SpeedTrackBar.Minimum = 100;
+            }
+        }
+
+        private void SimulateModeButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (SimulateModeButton.Checked)
+            {
+                CopyPasteMode = false;
+                if (SpeedTrackBar.Value < 250)
+                {
+                    SpeedTrackBar.Value = 250;
+                }
+                SpeedTrackBar.Minimum = 250;
+
+                if (Running)
+                {
+                    Timer.Change(0, SpeedTrackBar.Value);
+                }
+                SpeedLabel.Text = "Speed: " + SpeedTrackBar.Value + "ms";
+            }
+        }
+
+        private void SpamText_Change(object sender, EventArgs e)
+        {
+            if (!SpamText.Text.Equals(""))
+            {
+                Clipboard.SetText(SpamText.Text);
+            }
+            else
+            {
+                Clipboard.Clear();
+            }
+            SpamText2 = SpamText.Text;
         }
     }
 }
